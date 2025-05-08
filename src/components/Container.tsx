d88c802b7bc6ac05e10099e189b39f731be58197
vmaarcos <111014095+vmaarcos@users.noreply.github.com>
2025-05-08 12:38:12 -0300

    Update Container.tsx

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -8,7 +8,7 @@ interface ContainerProps {
 export function Container(props: Readonly<ContainerProps>) {
   return (
     <div
-      className={`container p-8 mx-auto xl:px-0 ${
+      className={`container ml-4 p-8 mx-auto xl:px-0 ${
         props.className ? props.className : ""
       }`}>
       {props.children}
