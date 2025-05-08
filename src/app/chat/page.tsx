265d09f6df5211527b93fee5bf711f6021c248f5
vmaarcos <111014095+vmaarcos@users.noreply.github.com>
2025-05-08 12:26:27 -0300

    Update page.tsx

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState, useRef, useEffect } from 'react'
-import { SendHorizontal } from 'lucide-react' /
+import { SendHorizontal } from 'lucide-react' 
 
 export default function Chat() {
   const [input, setInput] = useState('')
